aa126abb55710e492a854562b129212f6ff0e47b
chandu0101 <chandu.csu2010@gmail.com>
2016-12-29 11:18:46 +0530

    fix

--- src/main/g8/project/PublicationDetails.scala
+++ src/main/g8/project/PublicationDetails.scala
@@ -38,7 +38,7 @@ object PublicationDetails {
       scalacOptions += {
         val a = (baseDirectory in LocalRootProject).value.toURI.toString
         val g = "https://raw.githubusercontent.com/chandu0101/$name$/" + sys.process.Process("git rev-parse HEAD").lines_!.head
-        // TODO fix this line 
+        s"-P:scalajs:mapSourceURI:\$a->\$g/"
       }
     )
 
